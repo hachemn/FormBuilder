5589ad10baf7d2440a0209edd55f5b6c234992d8
hachemn <hachemnourhen8@gmail.com>
2023-12-25 00:01:59 +0100

    comm

--- backend/data/notes.js
+++ backend/data/notes.js
@@ -3,19 +3,16 @@ const notes = [
     _id: "1",
     nom: "nourhen",
     description: "3 eme annee licence ",
-    category: "univ",
   },
   {
     _id: "2",
     nom: "sarah",
     description: "1 ere annee licence ",
-    category: "univ",
   },
   {
     _id: "3",
     nom: "nawres",
     description: "3 eme annee licence ",
-    category: "univ",
   },
 ];
 module.exports = notes;
