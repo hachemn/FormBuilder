6d760a5b0fe4bda3e9b280d56025b94e8891e2d3
hachemn <hachemnourhen8@gmail.com>
2023-12-24 13:37:06 +0100

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const notes = require("./data/notes");
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config({ path: __dirname + "/../.env" });
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+const connectDB = require("./config/db");
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.json(note);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import "./App.css";
+
+function App() {
+  const [formFields, setFormFields] = useState([]);
+
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const items = [...formFields];
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setFormFields(items);
+  };
+
+  const addInputField = () => {
+    const label = prompt("Enter label for the input field");
+    const type =
+      prompt(
+        "Enter type for the input field (text, number, password , select, phone, email):"
+      ) || "text";
+
+    if (label) {
+      let options = null;
+      if (type === "select") {
+        const optionString = prompt("Enter options for the input ");
+        options = optionString.split(",").map((option) => option.trim());
+      }
+      setFormFields([
+        ...formFields,
+        {
+          id: formFields.length + 1,
+          type,
+          label,
+          options,
+          selectedOptions: [],
+        },
+      ]);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log("Form data submitted:", formFields);
+  };
+
+  return (
+    <div className="App">
+      <h1>Form Builder</h1>
+
+      <div>
+        <button onClick={addInputField}>Add</button>
+      </div>
+
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided) => (
+            <form ref={provided.innerRef} {...provided.droppableProps}>
+              {formFields.map((field, index) => (
+                <Draggable
+                  key={field.id}
+                  draggableId={field.id.toString()}
+                  index={index}
+                >
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <label>{field.label}</label>
+                      {field.type === "select" ? (
+                        <select>
+                          {field.options &&
+                            field.options.map((option, optionIndex) => (
+                              <option key={optionIndex} value={option}>
+                                {option}
+                              </option>
+                            ))}
+                        </select>
+                      ) : field.type === "phone" ? (
+                        <input
+                          type="tel"
+                          pattern="\d{8}"
+                          title="Please enter a valid 8-digit phone number"
+                        />
+                      ) : field.type === "email" ? (
+                        <input type="email" />
+                      ) : (
+                        <input type={field.type} />
+                      )}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+              <button type="button" onClick={handleSubmit}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default App;
